7e3d0ee57d1ee908c45c970908d5832801c772a6
lester <lester.lamsen@gmail.com>
2018-03-29 06:32:24 -0500

    installedl kaminari semantic ui theme
    page 3 results

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
 
   validates :product_name, :release_date, :product_status, :sales_price, presence: true
   mount_uploader :image, ImageUploader
-
+  paginates_per 3
   def self.keyword_search(keywords)
     keywords = '%' + keywords + '%'
     Product.where('product_name LIKE ?', keywords)
