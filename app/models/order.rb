625eac8e104e2f47d3f66bf1900a1dd6eaa28c0c
lester <lester.lamsen@gmail.com>
2018-04-05 23:07:20 -0500

    added a many to many relationships between order and product tables

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :products
+  has_and_belongs_to_many :products
   validates :total_price, :product_cost, numericality: true
   validates :order_status, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :original
+  has_and_belongs_to_many :orders
 
   validates :product_name, :release_date, :product_status, :sales_price, presence: true
   mount_uploader :image, ImageUploader
