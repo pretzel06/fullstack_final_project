18f732a37088b1274f7196f6b8ea80c9e2f4de82
lester <lester.lamsen@gmail.com>
2018-04-06 03:11:51 -0500

    fixed some typos

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -13,20 +13,20 @@ ActiveAdmin.register Order do
 # end
 permit_params :tracking_number, :order_status, :product_cost, :subtotal,
               :sales_tax, :total_price, :customer_id, :product_id, :product_name,
-              product_orders_attributes: [:product_id, :order_id, :_destroy]
+              product_orders_attributes: [:product_id, :product_name, :order_id, :_destroy]
 
 form do |f|
   f.semantic_errors *f.object.errors.keys
 
   f.inputs "Order" do
-    f.input :customer_id
+    f.input :customer
     f.input :tracking_number
     f.input :order_status
     f.input :total_price
     f.input :subtotal
     f.input :sales_tax
     f.has_many :product_orders, allow_destroy: true do |n_f|
-      n_f.input :product_name
+      n_f.input :product
     end
   end
 
